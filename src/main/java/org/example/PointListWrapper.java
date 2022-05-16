b7633f067263a86397293b5135beca4c1cb87dfb
Bjoern Haye <haye@synapsis.de>
2022-05-16 09:39:02 +0200

    x

---
+++ src/main/java/org/example/PointListWrapper.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.LinkedList;
+
+public class PointListWrapper {
+    private LinkedList<Point> points;
+
+    public PointListWrapper() {
+        this.points = new LinkedList<Point>();
+    }
+
+    public void add(Point point) {
+        points.add(point);
+    }
+
+    public void remove(int i) {
+        points.remove(i);
+    }
+
+    public void get(int i) {
+        points.get(i);
+    }
+
+    public void getPointTriple(int i) {
+        
+    }
+
+    public Point searchPoint(int x, int y) {
+        return null;
+    }
+}
